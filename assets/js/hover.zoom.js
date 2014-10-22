ebeb6ce7073b6e7fe3adbd82253a5bea34d95c85
Pedro <skyzzoor@gmail.com>
2014-10-22 14:48:39 +0100

    version16

--- assets/js/hover.zoom.js
+++ assets/js/hover.zoom.js
@@ -8,7 +8,7 @@
                 overlay: true,
                 overlayColor: '#2e9dbd',
                 overlayOpacity: 0.9,
-                zoom: 0,
+                zoom: 25,
                 speed: 300
             };
 
